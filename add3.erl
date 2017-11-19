a8024bc44a96a6ebf9a94d9190f4ee2e7933e6b8
Sandesh Soni <hello@SandeshSoni.com>
2017-11-19 15:43:16 +0530

    add print cast fn

--- add3.erl
+++ add3.erl
@@ -1,14 +1,18 @@
 -module(add3).
 -behaviour(gen_server).
 
--export([start/1, do/2]).
--export([init/1, handle_call/3]).
+-export([start/1, do/2, print/1]).
+-export([init/1, handle_call/3, handle_cast/2]).
 
 start(I) ->
   gen_server:start(add3, I, []).
 
 do(Apid, Msg) ->
   gen_server:start(Apid, Msg).
+
+%% added new
+print(Apid) ->
+  genserver:cast(Apid, print).
 
 init(I) ->
   {ok, I}.
@@ -22,3 +26,7 @@ handle_call({sub,X},_From, N0) ->
 handle_call(neg, _From, N0) ->
   N1 = -N0,
   {reply, N1, N1}.
+
+handle_cast(print, N) ->
+  io:format("Val: ~p\n", [N]),
+  {noreply, N}.
